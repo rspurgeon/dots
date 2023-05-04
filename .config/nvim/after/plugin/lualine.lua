b820ca569849042511b7501f054c63110048417b
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2023-05-03 22:11:59 -0500

    20230503

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -3,16 +3,40 @@ local custom_auto = require'lualine.themes.auto'
 custom_auto.normal.c.bg = custom_auto.normal.b.bg
 custom_auto.insert.c.bg = custom_auto.insert.b.bg
 
+local inactive_color = {
+    fg = custom_auto.normal.b.fg,
+    bg = custom_auto.normal.b.bg,
+}
+
 require('lualine').setup {
     extensions = {'neo-tree', 'fugitive'},
     options = {
-        theme = custom_auto
+        theme = custom_auto,
     },
     sections = {
         lualine_c = {
             { 'filename', path = 1, shorting_target = 25 }
         },
         lualine_x = { 'filetype' },
+    },
+    inactive_sections = {
+        lualine_a = {
+        },
+        lualine_b = {
+        },
+        lualine_c = {
+            {
+                'filename',
+                width = '100%',
+                color = inactive_color,
+            }
+        },
+        lualine_x = {
+        },
+        lualine_y = {
+        },
+        lualine_z = {
+        },
     }
 }
 
